9fa07caef04dddc68813a58fc4cb0b3831b058cb
natipedraza <nati_oric77@hotmail.com>
2022-11-11 20:12:37 +0100

    creando crud

---
+++ cars/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from .views import (BrandListView,BrandCreateView,BrandRetrieveView,
+BrandUpdateView,BrandDestroyView)
+
+
+
+urlpatterns = [
+    path('brands/', BrandListView.as_view(), name='brands'),
+    path('brands/create/', BrandCreateView.as_view(), name='brand_create'),
+    path('brands/<int:id>/', BrandRetrieveView.as_view(), name='brand'),
+    path('brands/<int:id>/update/', BrandUpdateView.as_view(), name='brand_update'),
+    path('brands/<int:id>/delete/', BrandDestroyView.as_view(), name='brand_delete')
+]
